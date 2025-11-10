0ca32502d973e589f6c03b175fe493c6f0b65af2
Libotean Bogdan <liboteanbogdan361@gmail.com>
2025-11-10 17:05:09 +0200

    Functie pentru decodificarea individului

--- ToH.py
+++ ToH.py
@@ -1,43 +1,58 @@
 import random
 
 # Reprezentarea individului si creearea populatiei initiale pentru problema Turnurilor din Hanoi
+# Tijele sunt notate ca 1, 2 si 3
 
-P1, P2, P3 = 1, 2, 3 # Cele trei tije
-
+# Codificarea mutarilor posibile
 MAPPING_MOVES = {
-    0: (P1, P2),
-    1: (P2, P1),
-    2: (P1, P3),
-    3: (P3, P1),
-    4: (P2, P3),
-    5: (P3, P2)
+    0: (1, 2),
+    1: (2, 1),
+    2: (1, 3),
+    3: (3, 1),
+    4: (2, 3),
+    5: (3, 2)
 }
 
-# lista tuturor mutarilor posibile
+# Lista tuturor mutarilor posibile
 ALL_MOVES = list(MAPPING_MOVES.keys())
 
 def creare_individ(n_disks, max_moves=None):
 
     if max_moves is None:
         optim_moves = (n_disks**2) - 1
-        max_moves = optim_moves + 1 # asiguram o mai mare sansa ca individul sa contina solutia optima
+        max_moves = optim_moves + 1 # asiguram o sansa mai mare ca individul sa contina solutia optima
 
     individ = [random.choice(ALL_MOVES) for _ in range(max_moves)]
 
     return individ
 
 # Generarea populatiei intiale
-
 def crearea_generatiei_initiale(n_disks, n_populatie, max_moves=None):
 
     populatie = []
-
     for _ in range(n_populatie):
         individ = creare_individ(n_disks, max_moves)
         populatie.append(individ)
 
     return populatie
 
-n = creare_individ(3)
-print(n)
+# Traduce mutarile din codificarea individului in mutarile efective
+def traducere_individ(individ):
 
+    mutari_traduse = []
+    for mutari in individ:
+        tuplu_mutari = MAPPING_MOVES.get(mutari)
+        mutari_traduse.append(tuplu_mutari)
+
+    return mutari_traduse
+
+
+
+pop_initiala = crearea_generatiei_initiale(3, 10)
+
+for individ in pop_initiala:
+    print(individ)
+
+# Afisare individ decodificat
+for individ in pop_initiala:
+    print(traducere_individ(individ))
