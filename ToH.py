55df5d06144e1001c322a444783441bae1a5aae6
Alex <vintalexandru03@gmail.com>
2025-11-10 19:36:29 +0200

    Update ToH.py

--- ToH.py
+++ ToH.py
@@ -17,7 +17,68 @@
 # CODE:
 # MUTATIE
 
+<<<<<<< Updated upstream
 Array_reprezentare=[]
 def mutatie():
     NrRandom = random(0,100)
     if(NrRandom <= 5):
+=======
+MAPPING_MOVES = {
+    0: (1, 2),
+    1: (2, 1),
+    2: (1, 3),
+    3: (3, 1),
+    4: (2, 3),
+    5: (3, 2)
+}
+
+# Lista tuturor mutarilor posibile
+ALL_MOVES = list(MAPPING_MOVES.keys())
+
+def creare_individ(n_disks, max_moves=None):
+
+    if max_moves is None:
+        optim_moves = (2**n_disks) - 1
+        max_moves = optim_moves + 1 # asiguram o sansa mai mare ca individul sa contina solutia optima
+
+    individ = [random.choice(ALL_MOVES) for _ in range(max_moves)]
+
+    return individ
+
+# Generarea populatiei intiale
+def crearea_generatiei_initiale(n_disks, n_populatie, max_moves=None):
+
+    populatie = []
+    for _ in range(n_populatie):
+        individ = creare_individ(n_disks, max_moves)
+        populatie.append(individ)
+
+    return populatie
+
+# Traduce mutarile din codificarea individului in mutarile efective
+def traducere_individ(individ):
+
+    mutari_traduse = []
+    for mutari in individ:
+        tuplu_mutari = MAPPING_MOVES.get(mutari)
+        mutari_traduse.append(tuplu_mutari)
+
+    return mutari_traduse
+
+# Mutatie in cazul in care fiecare gena are sansa de 5% de a fi modificata
+def mutatie_gena(individ):
+    for individ in pop_initiala:
+        for i in range(len(individ)):
+            nr_aleator = random.randint(0,100)
+            if(nr_aleator)<=5:
+                individ[i] = random.choice(ALL_MOVES)
+
+pop_initiala = crearea_generatiei_initiale(3, 5)
+for individ in pop_initiala:
+    mutatie_gena(individ)
+    print(individ)
+
+# Afisare individ decodificat
+# for individ in pop_initiala:
+#     print(traducere_individ(individ))
+>>>>>>> Stashed changes
