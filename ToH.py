c05dda03ca6d2a77d26de1234a039797e245844b
Alex <vintalexandru03@gmail.com>
2025-11-10 15:38:07 +0200

    Selectie Si mutatie

--- ToH.py
+++ ToH.py
@@ -1,8 +1,12 @@
+
+
+
+# Sectiune - Alex
 # Mutatie si selectie:
 
 # pentru mutatii ne trebuie un random(0,100) <=5 adica 5%
 # si atunci din arrayul de reprezentarea a individului parcurgand fiecare pozitie se executa functia de random si daca random e mai mic sau = cu 5 atunci se va intampla o mutatie (schimb cu o alta succesiune de mutari, random)
-
+asdasdas
 
 # SELECTIE
 # -sortez populatia in functie de fitness si aleg random cativa indivizi (am nevoie de un mecanism care da sansa mai buna celui cu fitness mai mare pentru a fi alesi) din care aleg random o pereche de parinti
