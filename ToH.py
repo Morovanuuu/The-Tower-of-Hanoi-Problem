445ff028122bd49902dd301f8b64628f77ddc9e5
Alex <vintalexandru03@gmail.com>
2025-11-10 15:57:13 +0200

    Update ToH.py

--- ToH.py
+++ ToH.py
@@ -1,9 +1,15 @@
 # Mutatie si selectie:
 
 # pentru mutatii ne trebuie un random(0,100) <=5 adica 5%
-# si atunci din arrayul de reprezentarea a individului parcurgand fiecare pozitie se executa functia de random si daca random e mai mic sau = cu 5 atunci se va intampla o mutatie (schimb cu o alta succesiune de mutari, random)
+# si atunci din arrayul de reprezentarea a individului parcurgand 
+# fiecare pozitie se executa functia de random si daca random e mai 
+# mic sau = cu 5 atunci se va intampla o mutatie (schimb cu o alta succesiune de mutari, random)
 
 
 # SELECTIE
-# -sortez populatia in functie de fitness si aleg random cativa indivizi (am nevoie de un mecanism care da sansa mai buna celui cu fitness mai mare pentru a fi alesi) din care aleg random o pereche de parinti
-# alta metoda e roata norocului unde fiecare individ are o felie dar felie e direct proportionala cu fitness ul si atunci cel care are fitnessul cel mai bun s-ar putea sa aiba felia 30% din roata iar cel mai slab poate sa aiba 1%
+# -sortez populatia in functie de fitness si aleg random cativa 
+# indivizi (am nevoie de un mecanism care da sansa mai buna celui cu fitness mai mare pentru a fi alesi) 
+# din care aleg random o pereche de parinti
+# alta metoda e roata norocului unde fiecare individ are o felie dar 
+# felie e direct proportionala cu fitness ul si atunci cel care are fitnessul 
+# cel mai bun s-ar putea sa aiba felia 30% din roata iar cel mai slab poate sa aiba 1%soun
