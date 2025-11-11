a4dc58155ea8b81f3841dd23497d9519944adce1
Moro <alexpaulmorovan@gmail.com>
2025-11-11 22:41:33 +0200

    Functia fitness

--- ToH.py
+++ ToH.py
@@ -14,3 +14,39 @@ def calculate_fitness(cromozom, num_disks, tija_initiala=0, tija_tinta=2):
     tije[tija_initiala] = list(range(num_disks, 0, -1))
 
     numar_mutari_invalide = 0
+
+    for (sursa, destinatie) in cromozom:
+
+        if sursa not in tije or destinatie not in tije or sursa == destinatie:
+            numar_mutari_invalide +=1
+            continue
+        #verific daca tija sursa e goala
+        if not tije[sursa]:
+            numar_mutari_invalide +=1
+            continue
+        #verific regula
+        if tije[destinatie]:
+            disc_sursa = tije[sursa][-1]
+            disc_destinatie = tije[destinatie][-1]
+
+            if disc_sursa > disc_destinatie:
+                numar_mutari_invalide +=1
+                continue
+
+
+        disc_de_mutat = tije[sursa].pop()
+        tije[destinatie].append(disc_de_mutat)
+
+    scor_stare=0
+    tija_finala = tije[tija_tinta]
+
+    for disc in tija_finala:
+        scor_stare += (2**(disc - 1))
+
+    penalizare_invalida = const_penalizare * numar_mutari_invalide
+    L = len(cromozom)
+    penalizare_lungime = const_lungime * max(0, L - lungime_optima)
+
+    fitness = scor_stare - penalizare_invalida - penalizare_lungime
+
+    return fitness
