c7d605268044c2a84e7a27b8dd5f85ab467c848e
Libotean Bogdan <liboteanbogdan361@gmail.com>
2025-11-19 22:59:55 +0200

    Functia de selectie

--- ToH.py
+++ ToH.py
@@ -1,7 +1,7 @@
 import random
 
 #  variabile globale
-numarIndivizi = 5
+numarIndivizi = 10
 numarDiscuri = 3
 indexSortare = 0 # varibila pentru sortarea tuplurilor in functie de fitness
 procentMutatie = 5
@@ -121,13 +121,13 @@ def crossover(parinte1, parinte2):
     return copil1, copil2
         
 # Sortare 
-def sort_tuples(tuples, key_idx):
+def sort_tupluri(tuples, key_idx):
     key_func = lambda x: x[key_idx]
     sorted_tuples = sorted(tuples, key=key_func, reverse=True)
     return sorted_tuples
 
 # Suma tuturor Fitness - ilor necesara pentru functia de selectie
-def sumaFitnessGeneratie(tuplu,sumaFitness = 0.0):
+def suma_fitness_generatie(tuplu,sumaFitness = 0.0):
     tuplu_minim = min(tuplu,key = lambda x : x[0])
     valoare_minima = tuplu_minim[0]
     offset = abs(valoare_minima) + 1
@@ -135,43 +135,77 @@ def sumaFitnessGeneratie(tuplu,sumaFitness = 0.0):
 
     for element in tuplu:
         sumaFitness += (element[0] + offset)
-    return sumaFitness
+    return sumaFitness, offset
 
 # Procentul fiecarui individ
-def procentRoata(index ,sumaFitness):
+def procent_roata(index ,sumaFitness):
     return round(index/sumaFitness,3)
+
+# Selectie roata
+def selectie(populatie, numarParinti):
+    listFitness = []
+
+    for individ in populatie:
+        tradus = traducere_individ(individ)
+        fitness = calculate_fitness(tradus, numarDiscuri)
+        listFitness.append((fitness, individ))
+    
+    sumaFitness, offset = suma_fitness_generatie(listFitness)
+
+    listProcentaj = []
+    probabilitateAcumulata = 0.0
+
+    for fitness, individ in listFitness:
+        fitnessAjustat = fitness + offset
+        probabilitateAcumulata += fitnessAjustat / sumaFitness
+        listProcentaj.append((probabilitateAcumulata, individ))
+
+    parintiSelectati = []
+
+    for _ in range(numarParinti):
+        rand = random.random()
+        for intrare in listProcentaj:
+            if rand <= intrare[0]:
+                parintiSelectati.append(intrare[1])
+                break
+    
+    return parintiSelectati
+
 
 pop_initiala = crearea_generatiei_initiale(numarDiscuri, numarIndivizi)
 list_fitness = []
 list_selectie = []
 list_procentaj = []
+
 for individ in pop_initiala:
     mutatie_gena(individ)
     tradus = traducere_individ(individ)
     #print(individ, end=' ')
-    fitness = calculate_fitness(tradus, 3)
+    fitness = calculate_fitness(tradus, numarDiscuri)
     #print(calculate_fitness(tradus,3))
-    list_fitness.append(fitness)
-    
+    list_fitness.append(fitness)  
     
 tuplu = list(zip(list_fitness, pop_initiala))
-sorted_tuplu = sort_tuples(tuplu, indexSortare)
-sumaTuturorFitnesi= sumaFitnessGeneratie(tuplu,indexSortare)  
-for i,j in sorted_tuplu:
+sumaTuturorFitnesi= suma_fitness_generatie(tuplu,indexSortare)  
 
-    list_procentaj.append(procentRoata(i,sumaTuturorFitnesi))
+for i,j in tuplu:
+    list_procentaj.append(procent_roata(i,sumaTuturorFitnesi[0]))
 
 tuplu = list(zip(list_fitness,list_procentaj,pop_initiala))
+sorted_tuplu = sort_tupluri(tuplu, indexSortare)
 
-for i,j,k in tuplu:
+for i,j,k in sorted_tuplu:
     print(i,j,k, end="\n")
 print("Suma fitnesi:")
-print(sumaFitnessGeneratie(tuplu))
+print(suma_fitness_generatie(tuplu))
 
+print("--- Perechi de parinti ---")
+parintiSelectati = selectie(pop_initiala, numarIndivizi)  
 
-
-
-
+for i, parinte in enumerate(parintiSelectati):
+    print(f"Parinte {i+1}: {parinte}")
+    if i % 2 != 0:
+        print("-----")
 
 #print(tuplu,end='\n')
 # Afisare individ decodificat
